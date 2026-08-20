86c82cdfb6390e31f7b68b8ed5b0a05b3cf015c3
sagaamiroj <sagaamiroj@gmail.com>
2026-08-20 21:45:23 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ PORT=${PORT:-80}
 
 sed -i "s/^Listen .*/Listen ${PORT}/" /etc/apache2/ports.conf
 
-sed -i "s/<VirtualHost \*:[0-9]\+>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+sed -i "s/<VirtualHost \*:[0-9]\+>/<VirtualHost *:${PORT}>/" \
+    /etc/apache2/sites-available/000-default.conf
 
 exec apache2-foreground

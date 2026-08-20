f0eec1ac0e560761fba7109b2091f7590f1b05a4
sagaamiroj <sagaamiroj@gmail.com>
2026-08-20 21:45:48 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,15 @@ set -e
 
 PORT=${PORT:-80}
 
+# Railway dynamic port
 sed -i "s/^Listen .*/Listen ${PORT}/" /etc/apache2/ports.conf
 
 sed -i "s/<VirtualHost \*:[0-9]\+>/<VirtualHost *:${PORT}>/" \
     /etc/apache2/sites-available/000-default.conf
+
+# Pastikan hanya satu Apache MPM yang aktif
+a2dismod mpm_event 2>/dev/null || true
+a2dismod mpm_worker 2>/dev/null || true
+a2enmod mpm_prefork 2>/dev/null || true
 
 exec apache2-foreground
